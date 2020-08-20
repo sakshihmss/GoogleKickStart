a0a780f1c654bfab1b3fff3c98906b6aa35fcbf6
Sakshi <sakhi691998@gmail.com>
2020-08-20 11:43:58 +0530

    Add files via upload

---
+++ 2016/RoundA/Q3.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    for(int i=1;i<=T;i++)
+    {
+        int M;
+        cin>>M;
+        vector<int> temp;
+        for(int j=0;j<=M;j++)
+        {
+            int p;
+            cin>>p;
+            temp.push_back(p);
+            
+        }
+        cout<<"Case #"<<i<<": "<<ans<<endl;
+    }
+    return 0;
+}
